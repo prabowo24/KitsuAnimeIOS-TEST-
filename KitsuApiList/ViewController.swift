db53eb7cd889bde35755f23551834b7dea10784f
crodoad <35281138+crodoad@users.noreply.github.com>
2019-01-23 11:31:30 +0700

    Initial Commit

---
+++ KitsuApiList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KitsuApiList
+//
+//  Created by Admin on 1/23/19.
+//  Copyright © 2019 Patranto Prabowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
